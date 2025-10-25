56df8c6ba35e1809873ea3bea6c23b46d05a2749
litxtech <sonertoprak@litxtech.com>
2025-10-25 17:00:07 +0300

    AAB boyutu optimize edildi - Google Play Store için küçültüldü

--- metro.config.js
+++ metro.config.js
@@ -16,4 +16,15 @@ config.resolver.assetExts.push(
   'webp'
 );
 
+// Bundle boyutunu küçültmek için optimizasyonlar
+config.transformer.minifierConfig = {
+  keep_fnames: false,
+  mangle: {
+    keep_fnames: false,
+  },
+};
+
+// Tree shaking için
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = config;
