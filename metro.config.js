c25f60abbeab3ab6df403a4ce34d069ffcdac579
litxtech <sonertoprak@litxtech.com>
2025-10-25 17:02:52 +0300

    Metro config düzeltildi - basit konfigürasyon

--- metro.config.js
+++ metro.config.js
@@ -16,15 +16,4 @@ config.resolver.assetExts.push(
   'webp'
 );
 
-// Bundle boyutunu küçültmek için optimizasyonlar
-config.transformer.minifierConfig = {
-  keep_fnames: false,
-  mangle: {
-    keep_fnames: false,
-  },
-};
-
-// Tree shaking için
-config.resolver.unstable_enablePackageExports = true;
-
 module.exports = config;
