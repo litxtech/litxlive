75d4cc93f67919ce684f2e1c880d5039d7f85cee
litxtech <sonertoprak@litxtech.com>
2025-10-25 17:57:49 +0300

    Bundle boyutu optimize edildi, Vercel konfigürasyonu düzeltildi

--- metro.config.js
+++ metro.config.js
@@ -16,4 +16,20 @@ config.resolver.assetExts.push(
   'webp'
 );
 
+// Bundle boyutunu küçültmek için optimizasyonlar
+config.transformer.minifierConfig = {
+  keep_fnames: false,
+  mangle: {
+    keep_fnames: false,
+  },
+  compress: {
+    drop_console: true,
+    drop_debugger: true,
+    pure_funcs: ['console.log', 'console.info', 'console.debug'],
+  },
+};
+
+// Tree shaking için
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = config;
